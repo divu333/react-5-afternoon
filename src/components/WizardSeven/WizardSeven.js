9ba483347209b6206c9dc02a0ced8c6e60c7d280
divya <divu333@gmail.com>
2019-01-19 11:57:24 -0700

    commit

--- src/components/WizardSeven/WizardSeven.js
+++ src/components/WizardSeven/WizardSeven.js
@@ -1,25 +1,39 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class WizardSeven extends Component {
+import { connect } from 'react-redux';
+import { updateAddress1, updateAddress2, updateAddress3 } from '../../ducks/reducer';
 
-    render(){
-        return(
-            <div className="parent-div">
-                <div className="vert-align">                    
-                    
-                    <p>Estimate your credit score</p> <br />
-                    
-                    <div className="row">
-                        <Link to="/wEight"><button onClick={this.props.updateCreditE}>Excellent</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditG}>Good</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditF}>Fair</button></Link>
-                        <Link to="/wEight"><button onClick={this.props.updateCreditP}>Poor</button></Link>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+class WizardNine extends Component {
+  render() {
+    const { updateAddress1, updateAddress2, updateAddress3 } = this.props;
+
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+            <p>What is your address?</p> <br />
+
+            <input type="text" placeholder="Line One" onChange={ ( e ) => updateAddress1( e.target.value ) } />
+            <input type="text" placeholder="Line Two" onChange={ ( e ) => updateAddress2( e.target.value ) } />
+            <input type="text" placeholder="Line Three" onChange={ ( e ) => updateAddress3( e.target.value ) } />
+        
+            <Link to="/wTen">
+              <button className="margin-btn"> Next </button>
+            </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default WizardSeven;
+function mapStateToProps( state ) {
+  const { addressOne, addressTwo, addressThree } = state;
+
+  return {
+    addressOne,
+    addressTwo,
+    addressThree
+  };
+}
+
+export default connect( mapStateToProps, { updateAddress1, updateAddress2, updateAddress3 } )( WizardNine );
